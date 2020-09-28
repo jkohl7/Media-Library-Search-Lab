da46cf75765c134e72ac37bdae08f71139ff89fb
BIT student <bitstudent@bitnet.wctc.edu>
2020-09-28 08:15:44 -0500

    MovieListing initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovieListing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
