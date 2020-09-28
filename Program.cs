bdd496553a79be022a3a1e7098d6ed5f90318058
BIT student <bitstudent@bitnet.wctc.edu>
2020-09-28 08:27:26 -0500

    Makes sure file exists

--- Program.cs
+++ Program.cs
@@ -14,7 +14,16 @@ namespace MovieListing
             var logger = NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
             logger.Info("Program started");
 
-            Console.WriteLine("Hello World!");
+            string file = "movies.csv";
+             // make sure movie file exists
+            if (!File.Exists(file))
+            {
+                logger.Error("File does not exist: {File}", file);
+            }
+            else
+            {
+                // TODO: create user menu
+            }
             
             logger.Info("Program ended");
         }
